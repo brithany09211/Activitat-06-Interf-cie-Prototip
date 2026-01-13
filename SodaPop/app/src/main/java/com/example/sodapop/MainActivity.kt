be92d39de0bc9c60b772162f5ef56ad39ff683f9
brithany09211 <smb09211@gmail.com>
2026-01-13 19:49:25 +0100

    main hecho

--- SodaPop/app/src/main/java/com/example/sodapop/MainActivity.kt
+++ SodaPop/app/src/main/java/com/example/sodapop/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.sodapop
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Cridar abans de super.onCreate() i setContentView()
+        val splashScreen = installSplashScreen()
+
+        // Opcional: Per mantenir-lo a la pantalla durant més temps (p. ex. carregar dades)
+        splashScreen.setKeepOnScreenCondition {
+            // Retorna 'true' per mantenir el splash screen, 'false' per amagar-lo
+            // Aquí hi aniria la teva lògica de càrrega
+            false
         }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
