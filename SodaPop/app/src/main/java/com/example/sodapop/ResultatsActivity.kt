cf9320a45538268d1c3f0110554a33ad0d3b33c9
brithany09211 <smb09211@gmail.com>
2026-01-18 23:35:29 +0100

    Terminat la activitat corregits errors y buscador funcional
    # Conflicts:
    #	SodaPop/app/src/main/java/com/example/sodapop/BuscadorActivity.kt
    #	SodaPop/app/src/main/java/com/example/sodapop/ResultatsActivity.kt
    #	SodaPop/app/src/main/res/drawable/config_svgrepo_com.xml
    #	SodaPop/app/src/main/res/drawable/edit_3_svgrepo_com.xml
    #	SodaPop/app/src/main/res/drawable/threedots.xml
    #	SodaPop/app/src/main/res/layout/activity_resultats_activity.xml
    #	SodaPop/app/src/main/res/layout/fragment_les_meves_receptes.xml

--- SodaPop/app/src/main/java/com/example/sodapop/ResultatsActivity.kt
+++ SodaPop/app/src/main/java/com/example/sodapop/ResultatsActivity.kt
@@ -3,7 +3,6 @@ package com.example.sodapop
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,22 +20,20 @@ class ResultatsActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        adapter = RecetaAdapter(DataSource.recetas) { receta ->
-            Toast.makeText(this, "Has clicat: ${receta.nombre}", Toast.LENGTH_SHORT).show()
+        val searchQuery = intent.getStringExtra("search_query") ?: ""
+        val recetasFiltradas = if (searchQuery.isEmpty()) {
+            DataSource.recetas
+        } else {
+            DataSource.recetas.filter { it.nombre.contains(searchQuery, ignoreCase = true) }
         }
 
-        recyclerView.adapter = adapter
-
-        val recetasFiltradas = DataSource.recetas.filter { it.nombre.contains("s", ignoreCase = true) }
-
-        // Actualizem el Adapter
         adapter = RecetaAdapter(recetasFiltradas) { receta ->
             Toast.makeText(this, "Has clicat: ${receta.nombre}", Toast.LENGTH_SHORT).show()
         }
         recyclerView.adapter = adapter
 
         findViewById<ImageView>(R.id.resultats_backbutton).setOnClickListener {
-            startActivity(Intent(this, BuscadorActivity::class.java))
+            finish()
         }
     }
 }
