10ec0cb3f5de8d46255e3d64c5e7f32fd12bb124
brithany09211 <smb09211@gmail.com>
2026-01-29 19:23:28 +0100

    kdoc complet

--- SodaPop/app/src/main/java/com/example/sodapop/RegistrarViewModel.kt
+++ SodaPop/app/src/main/java/com/example/sodapop/RegistrarViewModel.kt
@@ -3,16 +3,38 @@ package com.example.sodapop
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
+/**
+ * ViewModel per a la pantalla de Registre.
+ * La seva feina és guardar les dades i fer les comprovacions (lògica)
+ * perquè l'Activity només s'encarregui de pintar la pantalla.
+ */
 class RegistrarViewModel : ViewModel() {
-    // LiveData que indica si el registre és correcte
+    /**
+     * LiveData per saber si el registre ha anat bé.
+     * 1. _registreCorrecte (Privada): És Mutable, així que puc modificar-la des d'aquí dins.
+     * 2. registreCorrecte (Pública): És no mutable, així l'Activity només la pot llegir (observar) però no tocar.
+     */
     private val _registreCorrecte = MutableLiveData<Boolean>(false)
     val registreCorrecte: LiveData<Boolean> = _registreCorrecte
 
-    // LiveData per missatges d’error
+    /**
+     * LiveData per gestionar els missatges d'error.
+     * Si alguna dada està malament, posarem aquí el text de l'error
+     * i l'Activity el mostrarà
+     */
     private val _missatgeError = MutableLiveData<String>()
     val missatgeError: LiveData<String> = _missatgeError
 
+    /**
+     * Funció que rep les dades que ha escrit l'usuari i mira si són vàlides.
+     * En lloc de fer els "if/else" a l'Activity, els fem aquí.
+     *
+     * @param nom El nom de l'usuari.
+     * @param cognoms Els cognoms.
+     * @param correu Ha de tenir '@' i '.'.
+     * @param contrasenya Mínim 6 caràcters.
+     * @param repetirContrasenya Ha de ser igual que la contrasenya.
+     */
     fun validarDades(nom: String, cognoms: String, correu: String, contrasenya: String, repetirContrasenya: String) {
         when {
             nom.isBlank() -> {
